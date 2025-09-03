3bc1e6d84f37c5913de3f169574b90d7adf09cf0
Mussi <mussi@prodev.com.br>
2025-09-04 00:05:00 +0400

    refactor: simplify HomePage component by removing unnecessary props and streamline user session handling

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -6,16 +6,6 @@ export default async function Page() {
   // Get user session
   const session = await auth();
 
-  const backendUrl = process.env.BACKEND_URL;
-  const nextauthTrustHost = process.env.NEXTAUTH_TRUST_HOST;
-  const nextauthBasePath = process.env.NEXTAUTH_BASE_PATH;
-  const nodeEnv = process.env.NODE_ENV;
-
   // Render
-  return (
-    <HomePage
-      user={session?.user}
-      options={{ backendUrl, nextauthTrustHost, nextauthBasePath, nodeEnv }}
-    />
-  );
+  return <HomePage user={session?.user} />;
 }
